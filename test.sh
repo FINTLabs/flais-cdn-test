ee17355b25d1355a9e5ce0346066e2426a171043
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2022-12-12 15:22:06 +0100

    set back to early state

--- test.sh
+++ test.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-for row in $(jq --raw-output '.entrypoints[]' build/asset-manifest.json); do
-
-  if [[ $row =~ static/js/main.*.js ]]; then
-    az storage blob upload \
-      --file "build/$row" \
-      --container-name "alpha/fint-kontroll-appbar-pod" \
-      --account-name stflaiscdn \
-      --name main.js \
-      --content-cache-control max-age=31536000 \
-      --overwrite
-  fi
-
-  if [[ $row =~ static/css/main.*.css ]]; then
-    az storage blob upload \
-      --file "build/$row" \
-      --container-name "alpha/fint-kontroll-appbar-pod" \
-      --account-name stflaiscdn \
-      --name main.css \
-      --content-cache-control max-age=31536000 \
-      --overwrite
-  fi
-
-done
+echo "It's connected"

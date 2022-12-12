0a668fa435fbf0b84ebd3855d9bb3f37fccc7fbf
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2022-12-12 12:59:32 +0100

    try extended bash script

--- test.sh
+++ test.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "It's connected"
+for row in $(jq --raw-output '.entrypoints[]' build/asset-manifest.json); do
+
+  if [[ $row =~ static/js/main.*.js ]]; then
+    az storage blob upload \
+      --file "build/$row" \
+      --container-name "alpha/fint-kontroll-appbar-pod" \
+      --account-name stflaiscdn \
+      --name main.js \
+      --content-cache-control max-age=31536000 \
+      --overwrite
+  fi
+
+  if [[ $row =~ static/css/main.*.css ]]; then
+    az storage blob upload \
+      --file "build/$row" \
+      --container-name "alpha/fint-kontroll-appbar-pod" \
+      --account-name stflaiscdn \
+      --name main.css \
+      --content-cache-control max-age=31536000 \
+      --overwrite
+  fi
+
+done
